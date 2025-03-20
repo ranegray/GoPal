07a1330f32a94d90ec57ceea2c0a5c36be4736eb
Ben-Mast <benjaminmst@gmail.com>
2025-03-20 17:46:04 -0600

    Completed barebones w/ nav bar

---
+++ ProjectSourceCode/index.js
@@ -0,0 +1,101 @@
+const express = require('express');
+const app = express();
+const handlebars = require('express-handlebars');
+const Handlebars = require('handlebars');
+const path = require('path');
+const pgp = require('pg-promise')(); 
+const bodyParser = require('body-parser');
+const session = require('express-session'); 
+const bcrypt = require('bcryptjs');
+
+const hbs = handlebars.create({
+    extname: 'hbs',
+    layoutsDir: __dirname + '/views/layouts',
+    partialsDir: __dirname + '/views/partials',
+});
+
+const dbConfig = {
+    host: 'db', // the database server
+    port: 5432, // the database port
+    database: process.env.POSTGRES_DB, // the database name
+    user: process.env.POSTGRES_USER, // the user account to connect with
+    password: process.env.POSTGRES_PASSWORD, // the password of the user account
+};
+
+const db = pgp(dbConfig);
+
+db.connect()
+    .then(obj => {
+        console.log('Database connection successful');
+        obj.done();
+    })
+    .catch(error => {
+        console.log('ERROR:', error.message || error);
+    });
+
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.json());
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        saveUninitialized: false,
+        resave: false,
+    })
+);
+
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+
+app.use(express.static(path.join(__dirname, "extra_resources")));
+
+//Authentication Middleware
+const auth = (req, res, next) => {
+    if (!req.session.user) {
+        return res.redirect('/login');
+    }
+    next();
+};
+
+app.get('/login', (req, res) => {
+    res.render('pages/login');
+});
+
+app.get('/register', (req, res) => {
+    res.render('pages/register');
+});
+
+//TODO: Put middleware on all of these once the login / register pages and backend exist
+
+app.get('/', (req, res) => {
+    res.redirect('/home');
+});
+
+app.get('/home', (req, res) => {
+    res.render('pages/home');
+});
+
+app.get('/activity', (req, res) => {
+    res.render('pages/activity');
+});
+
+app.get('/social', (req, res) => {
+    res.render('pages/social');
+});
+
+app.get('/pal', (req, res) => {
+    res.render('pages/pal');
+});
+
+app.get('/settings', (req, res) => {
+    res.render('pages/settings');
+});
+
+
+//Ensure App is Listening For Requests
+app.listen(3000);
