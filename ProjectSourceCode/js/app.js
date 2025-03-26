22a45642ae4b8ed4fd1caeea658da18760f1905d
Ben-Mast <benjaminmst@gmail.com>
2025-03-26 10:28:53 -0500

    reduced number of script tags to one, imported helper functions into app.js

--- ProjectSourceCode/js/app.js
+++ ProjectSourceCode/js/app.js
@@ -1,11 +1,28 @@
-//Turns Active Button Blue
-document.addEventListener('DOMContentLoaded', function() {
-    const currentPath = window.location.pathname;
-    const activeLink = document.querySelector(`.sidebar-nav-button[href="${currentPath}"]`);
-    
-    if (activeLink) {
-        activeLink.classList.remove('bg-white', 'hover:bg-gray-300');
-        activeLink.classList.add('bg-blue-300', 'hover:bg-blue-400');
-    }
-});
+import { passwordErrorText } from './password_error.js';
+import { getLocation } from './geolocation.js';
 
+//Turns active nav bar button blue
+const navBar = document.getElementById('nav-bar');
+if(navBar){
+    document.addEventListener('DOMContentLoaded', function() {
+        const currentPath = window.location.pathname;
+        const activeLink = document.querySelector(`.sidebar-nav-button[href="${currentPath}"]`);
+        
+        if (activeLink) {
+            activeLink.classList.remove('bg-white', 'hover:bg-gray-300');
+            activeLink.classList.add('bg-blue-300', 'hover:bg-blue-400');
+        }
+    });
+}
+
+// Register Page
+if (window.location.pathname === '/register'){
+    //Password error text setup
+    passwordErrorText()
+}
+
+// Home page
+// TODO: Make changes to geolocation.js so that the weather API key is not exposed to the frontend and the repo
+if (window.location.pathname === '/home'){
+    getLocation()
+}
--- ProjectSourceCode/js/password_error.js
+++ ProjectSourceCode/js/password_error.js
@@ -1,13 +1,15 @@
-const registerForm = document.getElementById('register_form');
-const passwordField = document.getElementById('password');
-const errorMessage = document.getElementById('password-error');
-const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[#?!@$%^&*\-]).{8,}$/;
+export function passwordErrorText(){
+    const registerForm = document.getElementById('register_form');
+    const passwordField = document.getElementById('password');
+    const errorMessage = document.getElementById('password-error');
+    const passwordRegex = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[#?!@$%^&*\-]).{8,}$/;
 
-registerForm.addEventListener('submit', function(event) {
-    const password = passwordField.value;
-    const isPasswordValid = passwordRegex.test(password);
-    errorMessage.classList.toggle('hidden', isPasswordValid);
-    if (!isPasswordValid) {
-        event.preventDefault();
-    }
-});
+    registerForm.addEventListener('submit', function(event) {
+        const password = passwordField.value;
+        const isPasswordValid = passwordRegex.test(password);
+        errorMessage.classList.toggle('hidden', isPasswordValid);
+        if (!isPasswordValid) {
+            event.preventDefault();
+        }
+    });
+}
