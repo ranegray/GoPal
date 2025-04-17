9bb6afcfa9917359220c074224d7d7b17071f7fc
rane gray <75066470+ranegray@users.noreply.github.com>
2025-04-17 12:29:23 -0600

    Update db.js for render deployment

--- ProjectSourceCode/server/db.js
+++ ProjectSourceCode/server/db.js
@@ -1,8 +1,8 @@
 const pgp = require("pg-promise")();
 
 const dbConfig = {
-  host: "db", // the database server
-  port: 5432, // the database port
+  host: process.env.HOST, // the database server
+  port: process.env.PORT, // the database port
   database: process.env.POSTGRES_DB, // the database name
   user: process.env.POSTGRES_USER, // the user account to connect with
   password: process.env.POSTGRES_PASSWORD, // the password of the user account
